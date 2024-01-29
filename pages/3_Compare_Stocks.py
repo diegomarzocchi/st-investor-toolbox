a842114a0cf19ed634b9271da08c9b7690872f2c
Diego Marzocchi <133065796+diegomarzocchi@users.noreply.github.com>
2024-01-29 19:07:12 -0300

    Update 3_Compare_Stocks.py

--- pages/3_Compare_Stocks.py
+++ pages/3_Compare_Stocks.py
@@ -4,22 +4,17 @@ import plotly.express as px
 
 # Function to get historical stock data
 def compare_stocks(tickers, period):
-    """
-    Compare the performance of multiple stocks over a specified period.
-
-    Parameters:
-    - tickers (str): Space-separated stock ticker symbols.
-    - period (str): Historical data period.
-
-    Returns:
-    - pd.Series: Normalized stock prices for comparison.
-    """
-
-    df = yf.download(input_tickers, period=selected_period)
-    df = df.Close
-    df_normalized = (df / df.iloc[0] - 1) * 100
-
-    return df_normalized
+    try:
+        # Attempt to download historical stock data
+        df = yf.download(tickers, period=period)
+        df = df.Close
+        # Normalize the data for comparison
+        df_normalized = (df / df.iloc[0] - 1) * 100
+        return df_normalized
+    except:
+        # Handle errors and display an error message
+        st.error(f"Error fetching data.")
+        return None
 
 # Create the user interface with Streamlit
 st.set_page_config(
@@ -30,17 +25,18 @@ st.set_page_config(
 
 st.title("Compare Stocks")
 st.write(
-    "This function allows you to retrieve compare stocks performance."
+    "This function allows you to compare the performance of multiple stocks."
 )
 
 # Get user-input values
 col1, col2 = st.columns(2)
 with col1:
-    ticker1 = st.text_input("Ticker 1:", placeholder="e.g. AAPL")
-    ticker2 = st.text_input("Ticker 2:", placeholder="e.g. TSLA")
+    # User input for ticker symbols
+    ticker1 = st.text_input("Symbol 1:", placeholder="e.g. AAPL")
+    ticker2 = st.text_input("Symbol 2:", placeholder="e.g. TSLA")
 with col2:
-    ticker3 = st.text_input("Ticker 3:", placeholder="e.g. KO")
-    ticker4 = st.text_input("Ticker 4:", placeholder="e.g. SPY")
+    ticker3 = st.text_input("Symbol 3:", placeholder="e.g. KO")
+    ticker4 = st.text_input("Symbol 4:", placeholder="e.g. SPY")
 input_tickers = ticker1 + " " + ticker2 + " " + ticker3 + " " + ticker4
 
 period_options = ["7d", "15d", "1mo", "2mo", "3mo", "6mo", "1y", "2y", "3y", "5y", "10y"]
@@ -48,17 +44,16 @@ selected_period = st.selectbox("Select Period:", period_options)
 
 # Button to get historical stock price
 if st.button("Compare Stocks"):
-    # Use st.spinner to show a spinner while the function is executing
-    with st.spinner("Fetching data..."):
-        # Call the function to get the stock price
-        df_normalized = compare_stocks(input_tickers, selected_period)
-
-        # Show the results
-        fig = px.line(df_normalized)
-
-        # Additional chart layout settings
-        fig.update_layout(title_text="Comparative Percent Performance")
-        fig.update_yaxes(title="Percentage change")
-
-        # Show the results
-        st.plotly_chart(fig)
+    if not input_tickers.strip():
+        # Display an error if no ticker symbols are provided
+        st.error("Please enter at least one ticker symbol.")
+    else:
+        with st.spinner("Fetching data..."):
+            # Call the function to get the stock price
+            df_normalized = compare_stocks(input_tickers, selected_period)
+            if df_normalized is not None:
+                # Show the results if there are no errors
+                fig = px.line(df_normalized)
+                fig.update_layout(title_text="Comparative Percent Performance")
+                fig.update_yaxes(title="Percentage change")
+                st.plotly_chart(fig)
